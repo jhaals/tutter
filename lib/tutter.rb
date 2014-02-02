fc6f27fa61a11a58a631d1113e4ba058db12d5f2
Johan Haals <johan.haals@gmail.com>
2014-02-02 19:22:37 +0100

    Inline documentation
    Output url to source code on get requests

--- lib/tutter.rb
+++ lib/tutter.rb
@@ -16,6 +16,7 @@ class Tutter < Sinatra::Base
     set :config, YAML.load_file('/etc/tutter.yaml')
   end
 
+  # Return project settings from config
   def get_project_settings project
     settings.config['projects'].each do |p|
       return p if p['name'] == project
@@ -24,18 +25,20 @@ class Tutter < Sinatra::Base
   end
 
   post '/' do
+    # Github send data in JSON format, parse it!
     data = JSON.parse request.body.read
     project = data['repository']['full_name']
 
     conf = get_project_settings(project)
-
     return 'Project does not exist in tutter.conf' unless conf
 
+    # Setup octokit endpoints
     Octokit.configure do |c|
       c.api_endpoint = conf['github_api_endpoint']
       c.web_endpoint = conf['github_site']
     end
 
+    # Authenticate to Github
     begin
       client = Octokit::Client.new :access_token => conf['access_token']
     rescue Octokit::Unauthorized
@@ -44,7 +47,7 @@ class Tutter < Sinatra::Base
       return "Account for #{project} has been temporary locked down due to to many failed login attempts"
     end
 
-
+    # Load action
     action = Action.create(conf['action'],
                                  conf['action_settings'],
                                  client,
@@ -52,8 +55,14 @@ class Tutter < Sinatra::Base
                                  data)
 
     action.run
-
-    return 'Source code and documentation at https://github.com/jhaals/tutter'
+    # Github does not care about status codes or return values.
+    # Output url to source if someone is interested
+    'Source code and documentation at https://github.com/jhaals/tutter'
   end
+
+  get '/' do
+    'Source code and documentation at https://github.com/jhaals/tutter'
+  end
+
   run! if app_file == $0
 end
