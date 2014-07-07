6819b7866bf88d6b49a5ad5c3759ff8661320b3b
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-07-07 17:59:58 +0200

    Refactored Tutter to include the Github event name when calling an Action. Also fixed a few issues I discovered while debugging from Githubs console.

--- lib/tutter.rb
+++ lib/tutter.rb
@@ -30,13 +30,7 @@ class Tutter < Sinatra::Base
     false
   end
 
-  post '/' do
-    # Github send data in JSON format, parse it!
-    begin
-      data = JSON.parse request.body.read
-    rescue JSON::ParserError
-      error(400, 'POST data is not JSON')
-    end
+  def try_action event, data
     project = data['repository']['full_name'] || error(400, 'Bad request')
 
     conf = get_project_settings(project) || error(404, 'Project does not exist in tutter.conf')
@@ -60,10 +54,41 @@ class Tutter < Sinatra::Base
                                  conf['action_settings'],
                                  client,
                                  project,
+                                 event,
                                  data)
 
     status_code, message = action.run
+
     return status_code, message
+  end
+
+  post '/' do
+    event = env['HTTP_X_GITHUB_EVENT']
+
+    unless event
+      error(500, "Invalid request")
+    end
+
+    # Get a 200 OK message in the Github webhook history
+    # Previously this showed an error.
+    if event == "ping"
+      return 200, "Tutter likes this hook!"
+    end
+
+    # Github send data in JSON format, parse it!
+    begin
+      data = JSON.parse request.body.read
+    rescue JSON::ParserError
+      error(400, 'POST data is not JSON')
+    end
+
+    # Check actions with repo
+
+    if data['repository']
+      return try_action(event, data)
+    end
+
+    error(400, "Unsupported request: #{event}")
   end
 
   get '/' do
